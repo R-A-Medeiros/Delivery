0dc6aeea1e76f0deefabfdec1d879abc5f83bf0e
Rinaldo Alves <r.medeiros1206@gmail.com>
2025-07-29 00:39:24 -0300

    implementação do kafka

--- Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/Delivery.java
+++ Microservices/Delivery-Tracking/src/main/java/com/delivery/delivery/tracking/domain/model/Delivery.java
@@ -1,8 +1,12 @@
 package com.delivery.delivery.tracking.domain.model;
 
+import com.delivery.delivery.tracking.domain.event.DeliveryFulfilledEvent;
+import com.delivery.delivery.tracking.domain.event.DeliveryPickUpEvent;
+import com.delivery.delivery.tracking.domain.event.DeliveryPlacedEvent;
 import com.delivery.delivery.tracking.domain.exception.DomainException;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.domain.AbstractAggregateRoot;
 
 
 import java.math.BigDecimal;
@@ -15,10 +19,10 @@ import java.util.UUID;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 @Setter(AccessLevel.PRIVATE)
 @Getter
-public class Delivery {
+public class Delivery extends AbstractAggregateRoot<Delivery> {
 
     @Id
     @EqualsAndHashCode.Include
@@ -118,17 +122,26 @@ public class Delivery {
         verifyIfCanBePlaced();
         this.changeStatusTo(DeliveryStatus.WAITING_FOR_COURIER);
         this.setPlacedAt(OffsetDateTime.now());
+        super.registerEvent(
+                new DeliveryPlacedEvent(this.getPlacedAt(), this.getId())
+        );
     }
 
     public void pickUp(UUID courierId) {
         this.setCourierId(courierId);
         this.changeStatusTo(DeliveryStatus.IN_TRANSIT);
         this.setAssignedAt(OffsetDateTime.now());
+        super.registerEvent(
+                new DeliveryPickUpEvent(this.getAssignedAt(), this.getId())
+        );
     }
 
     public void markAsDelivered() {
         this.changeStatusTo(DeliveryStatus.DELIVERED);
         this.setFulfilledAt(OffsetDateTime.now());
+        super.registerEvent(
+                new DeliveryFulfilledEvent(this.getFulfilledAt(), this.getId())
+        );
     }
 
     public List<Item> getItems() {
